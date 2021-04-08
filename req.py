e34eea5986b90d5184658e3691f153db254e6884
Ваше Имя <you@example.com>
2021-04-08 20:47:37 +0600

    firsdt commit

--- req.py
+++ req.py
@@ -1,13 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
+def parser_i(url, teg, u_class):
+    # url = url
+    source = requests.get(url)
+    main_text = source.text
+    soup = BeautifulSoup(main_text, "html.parser")
+    link = soup.find(teg, {'class': u_class})
+    return link.text
 
-url = 'https://habr.com/ru/search/?q=python#h'
+print(parser_i('https://habr.com/ru/search/?q=python#h', 'a', 'post__title_link'))
 
-source = requests.get(url)
-main_text = source.text
-soup = BeautifulSoup(main_text, "html.parser")
+print(parser_i('https://habr.com/ru/search/?target_type=posts&order_by=relevance&q=doker&flow=', 'a', 'post__title_link'))
+print(parser_i('https://habr.com/ru/company/mailru/blog/550138/', 'span', 'post__title-text'))
 
-link = soup.find('a', {'class': 'post__title_link'})
-
-print(link.text)
